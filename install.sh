3d60ff2b99d430e7f652b89c55cfc03b9042d9af
tony-wang1990 <wangyatao678@gmail.com>
2026-01-03 02:00:04 +0800

    Add king-detective.db download

--- install.sh
+++ install.sh
@@ -10,10 +10,11 @@ cd /app/king-detective
 # 下载配置文件
 wget -q https://raw.githubusercontent.com/tony-wang1990/King-Detective/main/docker-compose.yml
 wget -q https://raw.githubusercontent.com/tony-wang1990/King-Detective/main/src/main/resources/application.yml
+wget -q https://raw.githubusercontent.com/tony-wang1990/King-Detective/main/king-detective.db
 
 # 启动服务
 docker-compose up -d
 
-echo "安装完成！"
+echo "安装完成�?
 echo "访问地址: http://$(curl -s ifconfig.me):9527"
 echo "默认账号: admin / admin123456"
